45bae872471fb5dce76a0b78f0841997c5ac408b
nsutanto <nicholas.sutanto@gmail.com>
2018-02-06 22:26:41 -0500

    feat: Complete Insertion Sort 1

--- InsertionSort/InsertionSort/Program.cs
+++ InsertionSort/InsertionSort/Program.cs
@@ -1,12 +1,75 @@
-﻿using System;
+﻿// https://www.hackerrank.com/challenges/insertionsort1/problem
 
-namespace InsertionSort
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
 {
-    class Program
+
+    static void insertionSort1(int n, int[] arr)
     {
-        static void Main(string[] args)
+        // Complete this function
+        for (int i = 0; i < arr.Count() - 1; i++)
         {
-            Console.WriteLine("Hello World!");
+            int curNum = arr[i];
+            int nextNum = arr[i + 1];
+
+            if (nextNum < curNum)
+            {
+
+                int numToInsert = nextNum;
+                int indexNextNum = i + 1;
+                bool isFound = false;
+                for (int j = indexNextNum; j > 0; j--)
+                {
+
+                    if (numToInsert < arr[j - 1])
+                    {
+                        arr[j] = arr[j - 1];         
+                    }
+                    else
+                    {
+                        arr[j] = numToInsert;
+                        isFound = true;
+                    }
+
+                    foreach (var item in arr)
+                    {
+                        Console.Write(item.ToString());
+                        Console.Write(" ");
+                    }
+                    Console.WriteLine();
+                    if (isFound) {
+                        break;
+                    }
+                }
+
+                if (!isFound) {
+                    arr[0] = numToInsert;
+                }
+                foreach (var item in arr)
+                {
+                    Console.Write(item.ToString());
+                    Console.Write(" ");
+                }
+            }          
         }
+    }
+
+    static void Main(String[] args)
+    {
+        //string nString = "10";
+        //string array = "2 3 4 5 6 7 8 9 10 1";
+
+        //string nString = "14";
+        //string array = "1 3 5 9 13 22 27 35 46 51 55 83 87 23";
+        string nString = "5";
+        string array = "2 4 6 8 3";
+
+        int n = Convert.ToInt32(nString);
+        string[] arr_temp = array.Split(' ');
+        int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        insertionSort1(n, arr);
     }
 }
