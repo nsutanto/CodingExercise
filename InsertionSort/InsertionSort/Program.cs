11b6d011fcaa90c3a0e1f9a813ba4fc7a4286116
nsutanto <nicholas.sutanto@gmail.com>
2018-02-06 22:38:23 -0500

    feat: Add Insertion Sort 2

--- InsertionSort/InsertionSort/Program.cs
+++ InsertionSort/InsertionSort/Program.cs
@@ -47,13 +47,62 @@ class Solution
 
                 if (!isFound) {
                     arr[0] = numToInsert;
+                    foreach (var item in arr)
+                    {
+                        Console.Write(item.ToString());
+                        Console.Write(" ");
+                    }
                 }
-                foreach (var item in arr)
-                {
-                    Console.Write(item.ToString());
-                    Console.Write(" ");
-                }
+
             }          
+        }
+    }
+
+    static void insertionSort2(int n, int[] arr)
+    {
+        // Complete this function
+        for (int i = 0; i < arr.Count() - 1; i++)
+        {
+            int curNum = arr[i];
+            int nextNum = arr[i + 1];
+
+            if (nextNum < curNum)
+            {
+
+                int numToInsert = nextNum;
+                int indexNextNum = i + 1;
+                bool isFound = false;
+                for (int j = indexNextNum; j > 0; j--)
+                {
+
+                    if (numToInsert < arr[j - 1])
+                    {
+                        arr[j] = arr[j - 1];
+                    }
+                    else
+                    {
+                        arr[j] = numToInsert;
+                        isFound = true;
+                    }
+
+                  
+                    if (isFound)
+                    {
+                        break;
+                    }
+                }
+
+                if (!isFound)
+                {
+                    arr[0] = numToInsert;
+                }
+            }
+            foreach (var item in arr)
+            {
+                Console.Write(item.ToString());
+                Console.Write(" ");
+            }
+            Console.WriteLine();
         }
     }
 
@@ -64,12 +113,19 @@ class Solution
 
         //string nString = "14";
         //string array = "1 3 5 9 13 22 27 35 46 51 55 83 87 23";
-        string nString = "5";
-        string array = "2 4 6 8 3";
+        //string nString = "5";
+        //string array = "2 4 6 8 3";
 
+        //int n = Convert.ToInt32(nString);
+        //string[] arr_temp = array.Split(' ');
+        //int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        //insertionSort1(n, arr);
+
+        string nString = "6";
+        string array = "1 4 3 5 6 2";
         int n = Convert.ToInt32(nString);
         string[] arr_temp = array.Split(' ');
         int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
-        insertionSort1(n, arr);
+        insertionSort2(n, arr);
     }
 }
