a855ae6539de7fc31956acef6deb53328299d638
nsutanto <nicholas.sutanto@gmail.com>
2018-03-04 00:24:36 -0500

    feat: Adding Test case on main program

---
+++ LeetCode/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LeetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            //WordPattern test = new WordPattern();
+            //test.TestWordPattern("abba", "dog dog dog dog");
+
+            IsAnagram test = new IsAnagram();
+            test.TestIsAnagram("ab", "ba");
+
+        }
+    }
+}
