65c70f043204a47c361bd5e4c3c98dc834a880fb
nsutanto <nicholas.sutanto@gmail.com>
2018-03-04 00:23:46 -0500

    feat: Contains Duplicate

---
+++ LeetCode/ContainsDuplicate.cs
@@ -0,0 +1,35 @@
+﻿//https://leetcode.com/problems/contains-duplicate/description/
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LeetCode
+{
+    public class ContainsDuplicate
+    {
+        public ContainsDuplicate()
+        {
+        }
+
+        public bool TestContainsDuplicate(int[] nums)
+        {
+            HashSet<int> hash = new HashSet<int>();
+
+            foreach(int num in nums)
+            {
+                if (!hash.Contains(num))
+                {
+                    hash.Add(num);
+                }
+                else
+                {
+                    return true;
+                }
+            }
+
+            return false;
+
+        }
+
+    }
+}
