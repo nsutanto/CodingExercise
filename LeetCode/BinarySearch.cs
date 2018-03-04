7bf9e574c98e12ff6673ba9839f45a3fb6cfe2a2
nsutanto <nicholas.sutanto@gmail.com>
2018-03-04 18:00:28 -0500

    feat: FindMin

---
+++ LeetCode/BinarySearch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace LeetCode
+{
+    public class BinarySearch
+    {
+        public BinarySearch()
+        {
+        }
+
+        // https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/description/
+        public int FindMin(int[] nums)
+        {
+            if (nums.Length == 0)
+            {
+                return 0;
+            }
+
+            if (nums.Length == 1)
+            {
+                return nums[0];
+            }
+
+            int start = 0;
+            int end = nums.Length - 1;
+
+            while (start < end)
+            {
+                if (nums[start] < nums[end])
+                    return nums[start];
+
+                int mid = (start + end) / 2;
+
+                if (nums[mid] >= nums[start])
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid;
+                }
+            }
+
+            return nums[start];
+        }
+    }
+}
