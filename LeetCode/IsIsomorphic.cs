20f70e5a39be4de541b68df8d38370ccfb7005e3
nsutanto <nicholas.sutanto@gmail.com>
2018-03-04 00:24:00 -0500

    feat: Is Isomorphic

---
+++ LeetCode/IsIsomorphic.cs
@@ -0,0 +1,36 @@
+﻿//https://leetcode.com/problems/isomorphic-strings/description/
+using System;
+using System.Collections.Generic;
+namespace LeetCode
+{
+    public class IsIsomorphic
+    {
+        public IsIsomorphic()
+        {
+        }
+
+        public bool TestIsIsomorphic(string s, string t)
+        {
+            Dictionary<char, char> dict = new Dictionary<char, char>();
+            char valueOut;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dict.TryGetValue(s[i], out valueOut))
+                {
+                    if (valueOut != t[i])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    dict.Add(s[i], t[i]);
+                }
+            }
+
+
+            return true;
+        }
+    }
+}
